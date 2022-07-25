637cd87bf5194fcbb73fe0b89bd3f51139bb2dce
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-07-25 17:44:38 +0530

    themeToggle

--- components/NavBar/ToggleLightDark.js
+++ components/NavBar/ToggleLightDark.js
@@ -19,9 +19,9 @@ function ToggleLightDark() {
         }
     }
     return (
-        <div onClick={toggle} className={`flex  relative w-14 bg-black h-7 p-1  rounded-2xl border-2 border-white`}>
-            <div className={`absolute m-[2px] top-0  w-5 h-5 bg-white rounded-2xl transition left-0 ease-linear duration-200 ${dark ? "translate-x-7" : "translate-x-0"}`}></div>
-
+        <div onClick={toggle} className={`relative w-14  h-7 p-1  rounded-2xl border-2 e ${dark ? 'bg-black border-whit' : 'bg-white border-yellow-200'}`}>
+            <div className={`absolute m-[2px] top-0  w-5 h-5  rounded-2xl transition left-0 ease-linear duration-200 ${dark ? "translate-x-7 bg-white" : "bg-yellow-300 translate-x-0 "}`}></div>
+            <div className={`transform duration-500   absolute w-4 h-4  right-3 top-0 rounded-3xl ${dark ? "translate-x-0 bg-black" : "-z-10 bg-transparent -translate-x-10"}`} ></div>
         </div>
     )
 }
