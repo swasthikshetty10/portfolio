2ca3e9e9c0fdcbb641e4cc9294ed3165fd76ea45
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-06 09:59:06 +0530

    minor fixes

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -3,35 +3,48 @@ import ToggleLightDark from './ToggleLightDark'
 import Image from 'next/image'
 import navigations from './navigations'
 import { ImCross } from "react-icons/im"
+import { FiDownload } from "react-icons/fi"
+import Link from 'next/link'
 function NavBar() {
     const [active, setActive] = useState(false)
     return (
-        <nav className=" bg-transperent backdrop-blur-xl px-3 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="group bg-transperent backdrop-blur-xl px-3 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="transfor ease-linear duration-300 container flex flex-wrap justify-between items-center mx-auto">
-                <a href="https://flowbite.com/" className=" flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Swasthik Shettylogo Logo" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Swasthik Shetty</span>
-                </a>
-                <div className="flex justify-center items-center gap-3 md:order-2">
+                <Link href="/">
+                    <a href="#" className=" flex items-center">
+                        <img src="/logo.png" className="group-hover:rotate-90 transition ease-in-out duration-1000 mr-1 h-6 sm:h-10" alt="Swasthik Shettylogo Logo" />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white tracking-widest ">SWASTHIK SHETTY</span>
+                    </a>
+                </Link>
+                <div className="flex justify-center items-center gap-3 lg:order-2">
+                    <a className="hidden xl:inline-flex items-center gap-1 font-semibold animated-underline tracking-widest  lg:after:bg-sky-700 lg:dark:after:bg-white uppercase   cursor-pointer lg:text-lg  py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-sky-700 lg:p-0 lg:dark:hover:text-white dark:text-slate-200 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                        Resume <FiDownload />
+                    </a>
                     <ToggleLightDark />
-                    <button onClick={() => { setActive(!active) }} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-slate-600 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-slate-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <button onClick={() => { setActive(!active) }} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-slate-600 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-slate-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         {!active ? <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                             : <ImCross />
                         }
                     </button>
                 </div>
-                <div className={`${active ? "block" : "hidden"} text-lg text-center justify-between items-center w-full md:flex md:w-auto md:order-1`} >
-                    <ul className="flex flex-col p-2 mt-4 backdrop-blur-lg bg-gray-50/50 rounded-lg border border-sky-300/50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-slate-800/50 md:dark:bg-transparent  dark:border-gray-700/50">
+                <div className={`${active ? "block" : "hidden"} text-lg text-center justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} >
+                    <ul className="flex flex-col p-2 mt-4 backdrop-blur-lg bg-gray-50/50 rounded-lg border border-sky-300/50 lg:flex-row lg:space-x-8 lg:mt-0 lg:text-sm lg:font-medium lg:border-0 lg:bg-transparent dark:bg-slate-800/50 lg:dark:bg-transparent  dark:border-gray-700/50">
                         {
                             navigations().map((ele, idx) =>
                                 <li className="relative" key={idx}>
-                                    <a href={ele.href} onClick={() => { setActive(false) }} className="animated-underline tracking-widest  md:after:bg-sky-700 md:dark:after:bg-white uppercase   cursor-pointer md:text-lg block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-sky-700 md:p-0 md:dark:hover:text-white dark:text-slate-200 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    <a href={ele.href} onClick={() => { setActive(false) }} className="animated-underline tracking-widest  lg:after:bg-sky-700 lg:dark:after:bg-white uppercase   cursor-pointer lg:text-lg block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-sky-700 lg:p-0 lg:dark:hover:text-white dark:text-slate-200 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
                                         {ele.name}
                                     </a>
                                 </li >
+
                             )
                         }
+                        <li>
+                            <a className="inline-flex w-full justify-center lg:hidden items-center gap-1  animated-underline tracking-widest  lg:after:bg-sky-700 lg:dark:after:bg-white uppercase   cursor-pointer lg:text-lg  py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-sky-700 lg:p-0 lg:dark:hover:text-white dark:text-slate-200 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                                Resume <FiDownload />
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
--- components/Skills/index.js
+++ components/Skills/index.js
@@ -10,7 +10,11 @@ export default function Home() {
     return (
         <div className={` bg-gradient-to-br dark:from-slate-900 dark:via-gray-900 dark:to-black  flex flex-col-reverse  md:flex-row overflow-hidden justify-center `}>
             <div className="border-t-2 border-slate-200 dark:border-slate-800 md:border-none  z-10   pt-20 h-lg sm:h-auto  text-center sm:text-start sm:pt-40 sm:pl-10 md:pl-20 md:pt-40 flex flex-col gap-10 md:gap-5   ">
-                <h1 className="text-4xl font-bold">My Skills</h1>
+                <a className=" animated-underline  md:after:bg-sky-700 md:dark:after:bg-slate-300 
+            tracking-wide hover:tracking-widest duration-300 transform ease-in-out
+             md:after:h-1 text-center text-5xl font-bold link-glow">
+                    My Skills
+                </a>
                 <div className="p-5 md:p-10">
 
                 </div>
