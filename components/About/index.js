5c2603d47250abb5824ddc5f4898873a00c2f269
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-07 11:18:48 +0530

    projects card

--- components/About/index.js
+++ components/About/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Fade } from 'react-awesome-reveal'
 
-
 function About() {
     return (
-        <div className='text-center p-5 md:p-10    justify-center  items-center  bg-gradient-to-tr from-teal-50 via-sky-100 to-sky-50 dark:from-slate-900/95 dark:via-slate-900 dark:to-black    bg-gray-300  w-full'>
+        <div className='
+            text-center p-5 md:p-10 justify-center items-center w-full
+            bg-gradient-to-tr from-teal-50 via-sky-100 to-sky-50 dark:from-slate-900/95 dark:via-slate-900 dark:to-black bg-gray-300'>
             <div className="py-8 md:py-4">
-
                 <a className="
-            animated-underline  md:after:bg-sky-700 md:dark:after:bg-slate-300 
-            tracking-wide hover:tracking-widest duration-300 transform ease-in-out
-             md:after:h-1  text-center text-5xl font-bold link-glow">
+                animated-underline  md:after:bg-sky-700 md:dark:after:bg-slate-300 
+                tracking-wide hover:tracking-widest duration-300 transform ease-in-out
+                md:after:h-1  text-center text-5xl font-bold link-glow">
                     About Me
                 </a>
 
@@ -19,7 +19,7 @@ function About() {
                 <Fade  >
                     <div className="
                     border-2 border-sky-600/50 hover:bg-gradient-to-l from-cyan-200/40 to-pink-100/40 dark:from-slate-900/10 dark:to-blue-900/10   ease-in-out  dark:border-slate-500/50
-                    md:group-hover:-translate-x-10 transform-color ease-liner delay-500 duration-500  p-5 md:p-10 rounded-xl max-w-screen-lg mx-auto backdrop-blur bg-slate-400/10 dark:bg-slate-800/10">
+                    md:group-hover:-translate-x-10 transform-color ease-liner delay-100 duration-500  p-5 md:p-10 rounded-xl max-w-screen-lg mx-auto backdrop-blur bg-slate-400/10 dark:bg-slate-800/10">
                         <div>
                             Hello! My name is Swasthik Shetty and I enjoy creating things that live on the internet.
                             My interest in development started back in 2018 when I used to play lot of video games and decided to build one, then along the way I stared exploring cybersecurity, machine learning and web development.
@@ -28,9 +28,9 @@ function About() {
                     </div>
                 </Fade>
                 <Fade  >
-                    <div className="
+                    <div className="  delay-100
                     border-2 border-sky-600/50 hover:bg-gradient-to-l from-cyan-200/40 to-pink-100/40 dark:from-slate-900/10 dark:to-blue-900/10   ease-in-out  dark:border-slate-500/50
-                    md:group-hover:translate-x-10 transform-color ease-liner delay-500 duration-500  p-5 md:p-10 rounded-xl max-w-screen-lg mx-auto backdrop-blur bg-slate-400/10 dark:bg-slate-800/10">
+                    md:group-hover:translate-x-10 transform-color ease-liner  duration-500  p-5 md:p-10 rounded-xl max-w-screen-lg mx-auto backdrop-blur bg-slate-400/10 dark:bg-slate-800/10">
                         <div>
                             Fast-forward to today, I build full-stack websites and over the time I have gained proficiency in React JS, Next JS and Django ,My main focus these days is building web applications, contributing to open-source community and learning about blockchain and metaverse
                         </div>
--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,43 +1,84 @@
 import React from 'react'
 import { Fade } from 'react-awesome-reveal'
-
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"
+import { Parallax } from 'react-scroll-parallax'
+import useBreakpoints from '../../hooks/useBreakpoint'
 
 function Projects() {
     return (
-        <div className='text-center p-5 md:p-10    justify-center  items-center  bg-gradient-to-tr from-teal-50 via-sky-100 to-sky-50 dark:from-slate-900/95 dark:via-slate-900 dark:to-black    bg-gray-300  w-full'>
+        <div className='text-center space-y-8 p-5 md:p-10 justify-center items-center bg-gradient-to-tr from-teal-50 via-sky-100 to-sky-50 dark:from-slate-900/95 dark:via-slate-900 dark:to-black bg-gray-300 w-full'>
             <div className="py-8 md:py-4">
                 <a className="
             animated-underline  md:after:bg-sky-700 md:dark:after:bg-slate-300 
             tracking-wide hover:tracking-widest duration-300 transform ease-in-out
-             md:after:h-1  text-center text-5xl font-bold link-glow">
+            md:after:h-1  text-center text-5xl font-bold link-glow">
                     Projects
                 </a>
             </div>
-            <div className="text-start  md:text-lg md:px-5 group space-y-6 md:space-y-8 md:p-10">
-                <div className='flex  flex-col  gap-5 md:gap-0 md:max-w-sm md:px-10  lg:max-w-md xl:max-w-xl group  mx-auto md:flex-row items-center justify-center
-                    border-2 border-sky-600/50 bg-gradient-to-tl text-base from-cyan-200/40 to-pink-100/50 dark:from-slate-900/50 dark:to-blue-900/50   ease-in-out  dark:border-slate-500/50
-                     transform-color ease-liner   p-3 md:p-5 rounded-xl  backdrop-blur  
-                    md:border-none md:bg-none
-                '>
-                    <div className=' flex-auto  md:translate-x-10 w-auto  md:p-0'>
-                        <img src="https://images.unsplash.com/photo-1558637845-c8b7ead71a3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                            className="md:max-w-lg md:grayscale transition-colors transform duration-400 ease-in-out group-hover:grayscale-0 "
-                        />
-                    </div>
-                    <div className="min-w-fit order-first md:order-2 space-y-5 md:text-end md:-translate-x-10">
-                        <h2 className="font-semibold text-2xl uppercase tracking-wider md:order-2">Project Name</h2>
-                        <div className="md:-ml-28 lg:-ml-26 xl:-ml-10
-                    md:border-2 border-sky-600/50 md:bg-gradient-to-tl text-base from-cyan-200/40 to-pink-100/50 dark:from-slate-900/70 dark:to-blue-900/40   ease-in-out  dark:border-slate-500/50
-                     transform ease-liner   p-2 md:p-5 rounded-xl  mx-auto backdrop-blur-md ">
 
-                            Fast-forward to today, I build full-stack websites and over the time I have gained proficiency in React JS, Next JS and Django ,My main focus these days is building web applications, contributing to open-source community and learning about blockchain and metaverse
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProjectsCard
+                name={<>Portfolio<span className="text-base ml-1 capitalize">(This website)</span></>}
+                tech={["React JS", "Next JS", "Django"]}
+                src="/projects/portfolio.png"
+                description={"Amet ut ipsum officia aute. Consectetur aliquip labore est minim id quis voluptate sit aliqua aute nisi do adipisicing. Quis veniam amet proident consectetur ex elit. Qui qui dolore in ea enim do ullamco."}
+            >
+            </ProjectsCard>
+            <ProjectsCard
+                name={<>Portfolio<span className="text-base ml-1 capitalize">(This website)</span></>}
+                tech={["React JS", "Next JS", "Django"]}
+                src="/projects/portfolio.png"
+                description={"Amet ut ipsum officia aute. Consectetur aliquip labore est minim id quis voluptate sit aliqua aute nisi do adipisicing. Quis veniam amet proident consectetur ex elit. Qui qui dolore in ea enim do ullamco."}
+            >
+            </ProjectsCard>
+            <ProjectsCard
+                reverse={true}
+                name={<>Portfolio<span className="text-base ml-1 capitalize">(This website)</span></>}
+                tech={["React JS", "Next JS", "Django"]}
+                src="/projects/portfolio.png"
+                description={"Amet ut ipsum officia aute. Consectetur aliquip labore est minim id quis voluptate sit aliqua aute nisi do adipisicing. Quis veniam amet proident consectetur ex elit. Qui qui dolore in ea enim do ullamco."}
+            >
+            </ProjectsCard>
         </div >
     )
 }
 
 export default Projects
+
+function ProjectsCard(props) {
+    const { isXs, isSm } = useBreakpoints();
+    return (<div className="text-start  md:text-lg md:px-5 group space-y-6 md:space-y-8 md:p-10">
+        <div className='flex  flex-col  gap-5 md:gap-0 md:max-w-sm md:px-10  lg:max-w-md xl:max-w-xl group  mx-auto md:flex-row items-center justify-center
+                    border-2 border-sky-500/50 bg-gradient-to-tl text-base from-cyan-100/40 to-gray-100/50 dark:from-slate-900/50 dark:to-gray-900/50   ease-in-out  dark:border-slate-500/50
+                     transform-color ease-liner   p-3 md:p-5 rounded-xl  backdrop-blur  
+                    md:border-none md:bg-none
+                '>
+            <div className={` flex-auto  ${props.reverse ? "md:-translate-x-10" : "md:translate-x-10"}  w-auto  md:p-0`}>
+                <img src={props.src}
+                    className="md:max-w-lg md:grayscale transition-colors transform duration-400 ease-in-out group-hover:grayscale-0 "
+                />
+            </div>
+            <div className={`min-w-fit order-first   ${props.reverse ? "md:order-first md:translate-x-10" : " md:text-end md:order-2 md:-translate-x-10"}`}>
+                <Parallax speed={isXs || isSm ? 0 : 13} className="space-y-2">
+                    <h2 className="hover:text-sky-800 dark:hover:text-sky-300 cursor-pointer font-semibold text-2xl uppercase  tracking-wider md:order-2">{props.name}</h2>
+                    <div className='space-x-3  md:tracking-wide  text-sm '>
+                        {props.tech.map((ele, i) => <a key={i}>{ele}</a>)}
+                    </div>
+                    <div className={`${props.reverse ? "md:-mr-28 lg:-mr-26 xl:-mr-10" : "md:-ml-28 lg:-ml-26 xl:-ml-10"}
+                md:border-2 border-sky-700 /20 md:bg-gradient-to-tl md:text-sm from-cyan-200/40 to-gray-100/90 dark:from-slate-900/70 dark:to-gray-900/70   ease-in-out  dark:border-slate-500/50
+                    transform ease-liner    md:p-5  rounded-xl  mx-auto md:backdrop-blur-md `}>
+                        {props.description}
+                    </div>
+                    <div className="inline-flex gap-3 pt-3 text-2xl ">
+                        <a href={props.github}>
+                            <FaGithub className="hover:text-sky-800 dark:hover:text-sky-300  " />
+                        </a>
+                        <a href={props.link}>
+                            <FaExternalLinkAlt className="hover:text-sky-800 dark:hover:text-sky-300 " />
+                        </a>
+
+                    </div>
+                </Parallax>
+            </div >
+        </div >
+    </div >)
+}
