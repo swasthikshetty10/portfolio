53f4d5144a3a4498346a157eaa274fa0f57d8dfa
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-10 19:20:02 +0530

    loader

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Loader() {
     return (
-        <div className='absolute top-0 left-0 w-screen h-screen bg-slate-900 z-[9999] text-center flex items-center justify-center'>
+        <div className='fixed  top-0 right-0 left-0   w-full h-screen bg-slate-900 z-[9999] text-center flex items-center justify-center'>
             <div>
                 <img className="w-14 md:w-28 h-auto animate-spin" src="/logo.png" />
-                <p className='p-3 text-2xl  text-slate-100'>Loading...</p>
+                <p className='p-3 text-2xl   text-slate-100'>Loading...</p>
             </div>
         </div>
     )
