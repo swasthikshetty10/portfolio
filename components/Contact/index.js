8c6a74b8fc3eb2fb9e7e72237c185494dd36b8d1
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-08-10 11:00:52 +0530

    Contact

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,35 +1,54 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { HiOutlineMail } from "react-icons/hi"
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+import Link from 'next/link'
+import { Zoom } from 'react-awesome-reveal'
 function Experience() {
+    const [sending, setSending] = useState(false);
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setSending(true);
+
+    }
     return (
 
         <section className='p-5 md:p-10 bg-gradient-to-br  w-full'>
-            <form className="p-2 justify-center gap-5 md:gap-10 flex flex-col md:flex-row md:p-10 mx-auto min-w-fit">
-                <div className="space-y-8 text-center">
+            <form onSubmit={submitHandler} className="group p-2 justify-center items-center md:items-start gap-5 md:gap-10 lg:gap-14 flex flex-col md:flex-row md:p-10 mx-auto min-w-fit">
+                <div className="space-y-5 text-center">
                     <h2 className='text-4xl font-semibold'>Let&#39;s Get In touch</h2>
-
-                    <img className="w-full max-w-sm hover:scale-90 hover:translate-y-5 duration-500 transform ease-in-out" src="/contact.svg" />
+                    <div className="text-3xl flex flex-wrap justify-center gap-5 ">
+                        <Link href="#"><a className="hover:scale-110 transition duration-400 ease-in-out" target="_blank"><HiOutlineMail /></a></Link>
+                        <Link href="#"><a className="hover:scale-110 transition duration-400 ease-in-out" target="_blank"><FaWhatsapp /></a></Link>
+                        <Link href="#"><a className="hover:scale-110 transition duration-400 ease-in-out" target="_blank"><FaLinkedin /></a></Link>
+                        <Link href="#"><a className="hover:scale-110 transition duration-400 ease-in-out" target="_blank"><FaGithub /></a></Link>
+                    </div>
+                    <Zoom delay={300}>
+                        <img className="hidden md:block pt-6 w-full max-w-sm " src="/contact.svg" />
+                    </Zoom>
                 </div>
-                <div className="flex-1  max-w-lg">
+
+                <div className="flex-1  max-w-sm space-y-6 ">
                     <div>
                         <span className="uppercase text-sm  font-bold">Full Name</span>
-                        <input className="w-full bg-slate-300/50 focus:border-2 border-opacity-50 border-sky-600 dark:border-slate-500 dark:bg-slate-300/10 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        <input disabled={sending} className="w-full bg-slate-300/50 focus:border-2 border-opacity-50 border-sky-600 dark:border-slate-500 dark:bg-slate-300/10 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                             type="text" placeholder="" />
                     </div>
-                    <div className="mt-8">
+                    <div >
                         <span className="uppercase text-sm  font-bold">Email</span>
-                        <input className="w-full bg-slate-300/50 focus:border-2 border-opacity-50 border-sky-600 dark:border-slate-500 dark:bg-slate-300/10 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                        <input disabled={sending} className="w-full bg-slate-300/50 focus:border-2 border-opacity-50 border-sky-600 dark:border-slate-500 dark:bg-slate-300/10 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                             type="text" />
                     </div>
-                    <div className="mt-8">
+                    <div >
                         <span className="uppercase text-sm  font-bold">Message</span>
-                        <textarea
+                        <textarea disabled={sending}
                             className="w-full h-32 bg-slate-300/50 focus:border-2 border-opacity-50 border-sky-600 dark:border-slate-500 dark:bg-slate-300/10 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
                     </div>
-                    <div className="mt-8">
-                        <button className="w-full rounded-2xl md:rounded-3xl p-3 sm:p-4 border-2 md:border-4 cursor-pointer select-none text-xl sm:text-xl font-semibold border-sky-600 hover:bg-gradient-to-l from-cyan-200/50 to-pink-100/50 dark:from-slate-900/50 dark:to-blue-900/50  transform ease-in-out duration-500 dark:border-slate-500   ">Send Message</button>
+                    <div >
+                        <button disabled={sending} className="disabled:opacity-70  w-full rounded-2xl md:rounded-3xl p-3  border-2 md:border-4 cursor-pointer select-none text-xl sm:text-xl font-semibold border-sky-600 hover:bg-gradient-to-l from-cyan-200/50 to-pink-100/50 dark:from-slate-900/50 dark:to-blue-900/50  transform ease-in-out duration-500 dark:border-slate-500   inline-flex justify-center gap-2 items-center">{sending ? <> <AiOutlineLoading3Quarters className="animate-spin" />  Sending </> : "Send Message"}</button>
                     </div>
                 </div>
+
             </form>
         </section>
     )
